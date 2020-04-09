e5fdafcd0ecb3baa308f402f99f8bf2061296ac3
Ben Rachbach <brachbach@gmail.com>
2020-04-09 15:04:35 -0700

    Adding growth rates to plague_plus model

--- plague_plus_model.js
+++ plague_plus_model.js
@@ -4,10 +4,30 @@
 // Assumptions are explained in the model sheet: https://docs.google.com/spreadsheets/d/1sYLHA61Uvocxvq5os26u5luxMpo9QQcsZwe13_tU_FE/edit#gid=371207754
 assumptions = {
     // 0.015 is the average death rate from 'Assumptions'!C11 
+    // Note from model: "We had been estimating 1%, but Korea's recent rise (presumably due to people not having had time to die) indicates that number may not be conservative. (In this model, the lower the death rate assumption, the higher the number of estimated cases.) Based on Diamond Princess rates at about 1% and SK at 1.6% (but likely not having detected 100% of cases) I still think overall rate is likely beloe 1%, but I'm bumping this up to maintain conservatism."
     infections_per_death: 1/0.015,
 
-    // 'Assumptions'!C10 
-    days_from_infection_to_death: 21
+    // 'Assumptions'!C10
+    // Note from model: "Naively it looks like this may need to be 23 days, but a more careful examination is needed before bumping it up further."
+    days_from_infection_to_death: 21,
+    infection_growth: {
+        // 'Assumptions'!C6
+        // Note from model: "Death growth rates are probably higher than infection growth rates currently due to deaths speeding up as hopspitals are overwhelmed"
+        uncountrolled_and_unaware: 0.24,
+
+        // 'Assumptions'!C7
+        // Note from model: "After a while people got more awareness and transmission rates probably went down."
+        uncountrolled_and_aware: 0.20,
+
+        // 'Assumptions'!C26
+        // Note from model: "For now, just using the numbers China seemed to achieve in the first 14 days after intervention"
+        // My note: this is based on the growth rate of deaths of Chinese people who got infected during days 1-14 of their intervention
+        intervention_day_1_to_14: 0.075,
+
+        // 'Assumptions'!C27
+        // My note: this is based on the growth rate of deaths of Chinese people who got infected during days 15+ after their lockdown
+        intervention_day_15_plues: 0.068514
+    }
 }
 
 
@@ -25,7 +45,7 @@ intermediate_values = {
     // 'Infection Growth Rates'
     // I'm not sure what "infection growth" means here. It's definitely not just growth in confirmed cases
     // I'd think it's a guess at growth in number of people infected so far
-    argentina_daily_infection_growth_rate: [0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2360000, 0.2320000, 0.2280000, 0.2240000, 0.2200000, 0.2160000, 0.2120000, 0.2080000, 0.2040000, 0.2000000, 0.2000000, 0.2000000, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514]
+    argentina_daily_infection_growth_rate_last_month_or_so: [0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2400000, 0.2360000, 0.2320000, 0.2280000, 0.2240000, 0.2200000, 0.2160000, 0.2120000, 0.2080000, 0.2040000, 0.2000000, 0.2000000, 0.2000000, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0750019, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514, 0.0068514]
 }
 
 
